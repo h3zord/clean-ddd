25377962ac202db04943c547d43271b2cf164a27
Lucas <lucaschmain@gmail.com>
2024-10-29 14:47:35 -0300

    feat: adding create question use case

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [tsConfigPaths()],
+  test: {
+    globals: true,
+  },
+})
